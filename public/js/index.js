8fecb09df46a30f9e2ef322b3218448e0c60e067
yurim99 <me_28@naver.com>
2023-10-26 12:21:03 +0900

    first commit

---
+++ public/js/index.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".work-wrapper .mySwiper", {
+	loop: true,
+	loopFillGroupWithBlank: true,
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	}
+});
